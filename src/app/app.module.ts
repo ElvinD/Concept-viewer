08b2be564e7c523ae9e5ed04a28b00f47940d4e3
Elvin Dechesne <elvin.dechesne@ordina.nl>
2021-07-22 17:56:17 +0200

    Angular Tree example added for future reference

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon'
 import { MatTreeModule } from '@angular/material/tree';
 import { MatButtonModule  } from '@angular/material/button';
+import { ConceptlistComponent } from './conceptlist/conceptlist.component';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListResourcesComponent
+    ListResourcesComponent,
+    ConceptlistComponent
   ],
   imports: [
+    MatProgressBarModule,
     BrowserModule,
     MatButtonModule,
     MatTreeModule,
