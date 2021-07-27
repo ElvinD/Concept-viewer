f432bb0c715c3da159a70d8d2147f6d7fb6942d9
ElvinD <elvin.dechesne@ordina.nl>
2021-07-27 16:34:53 +0200

    Lookup for labels toegevoegd in nodeMap Refactoring imminent...

--- src/app/conceptlist/conceptlist.component.ts
+++ src/app/conceptlist/conceptlist.component.ts
@@ -34,48 +34,14 @@ export class DynamicDatabase {
     return this.rootLevelNodes.map(name => new DynamicFlatNode(name, 0, true));
   }
 
-  // loadInitialData() {
-  //   return this.apollo.watchQuery<Query>({
-  //     query: GET_ConceptSchemes,
-  //     variables: {},
-  //   }).valueChanges.subscribe((response) => {
-  //     this.rootLevelNodes = response.data.conceptSchemes.map((scheme) => {
-  //       this.nodeMap.set(scheme.uri, {uri: scheme.uri, label: scheme.label, type: scheme.type})
-  //       return scheme.uri;
-  //     });
-  //   })
-  // };
-
-
-
-  // this.rootLevelConceptSchemes$.subscribe((data): void => {
-  //   this.database.rootLevelNodes = data.map(conceptScheme => {
-  //     const children = conceptScheme.hasTopConcept?.map(skos => {
-  //       return skos;
-  //     });
-  //     if (children) {
-  //       this.database.dataMap.set(conceptScheme.uri, children.map(child => child.uri));
-  //       children.map((child) => {
-  //         // console.log("iteraring children: ", child);
-  //         if (child.narrower?.length)
-  //           this.database.dataMap.set(child.uri, child.narrower.map(nestedChild => nestedChild.uri));
-  //       });
-  //     }
-  //     return conceptScheme.uri;
-  //   });
-  //   this.dataSource.data = this.database.initialData();
-  //   // console.log(this.database.dataMap);
-  // });
-  // }
-  // }
   async loadChildren(uri: string): Promise<string[]> {
-    console.log("get children from :", uri);
+    // console.log("get children from :", uri);
     return this.apollo.query<Query>({
       query: GET_Concepts,
       errorPolicy: 'all',
       variables: { filter_broader: uri }
     }).toPromise().then(result => {
-      console.log("loaded children:", result.data);
+      // console.log("loaded children:", result.data);
       if (result.data.concepts) {
         this.dataMap.set(uri, result.data.concepts.map(skos => {
           return skos.uri;
@@ -83,6 +49,11 @@ export class DynamicDatabase {
         result.data.concepts.map(skos => {
           if (skos.narrower && skos.narrower.length) {
             this.dataMap.set(skos.uri, skos.narrower?.map(skosChild => {
+              if(!this.nodeMap.get(skosChild.uri)) {
+                this.nodeMap.set(skosChild.uri, { uri: skosChild.uri, type: skosChild.type, label: skosChild.label });
+              } else {
+                console.log("bevat al skos element: ", skosChild);
+              }
               return skosChild.uri;
             }));
           }
@@ -118,27 +89,12 @@ export class DynamicDatabase {
       return resolve(this.dataMap.get(uri));
     });
   }
-  // return this.apollo.query<Query>({
-  //   query: GET_Concepts,
-  //   errorPolicy: 'all',
-  //   variables: { filter_broader: uri }
-  // }).subscribe((result => {
-  //   console.log("get children: ", result);
-  // }))
-  // if (this.dataMap.has(uri)) {
-  //   return new Promise((resolve): void => {
-  //     return resolve(this.dataMap.get(uri));
-  //   });
-  // } else {
-  //   console.log("moet laden", uri);
-  //   return new Promise(resolve => {
-  //     setTimeout(() => {
-  //       return resolve(this.dataMap.get(uri));
-  //     }, 1000);
-  //   });
 
-  // }
-  // }
+  getNode(uri:string):RDFNode | undefined {
+    const node = this.nodeMap.get(uri);
+    return node;
+  }
+  
 
   async hasChildren(uri: string): Promise<boolean> {
     return this.dataMap.has(uri);
@@ -182,18 +138,6 @@ export class DynamicDataSource implements DataSource<DynamicFlatNode> {
 
   disconnect(collectionViewer: CollectionViewer): void { }
 
-  /** Handle expand/collapse behaviors */
-  // handleTreeControl(change: SelectionChange<DynamicFlatNode>) {
-  //   if (change.added) {
-  //     change.added.forEach(node => {
-  //       return this.toggleNode(node, true);
-  //     });
-  //   }
-  //   if (change.removed) {
-  //     change.removed.slice().reverse().forEach((node): void => this.toggleNode(node, false));
-  //   }
-  // }
-
   handleTreeControl(change: SelectionChange<DynamicFlatNode>) {
     if (change.added) {
       change.added.forEach((node) => this.toggleNode(node, true));
@@ -210,8 +154,6 @@ export class DynamicDataSource implements DataSource<DynamicFlatNode> {
     } else {
       node.isLoading = true;
       children = await this._database.getChildren(node.item);
-      // const test = await this._database.getChildren(node.item);
-      // return;
     }
     const index = this.data.indexOf(node);
     if (!children || index < 0) { // If no children, or cannot find the node, no op
@@ -243,69 +185,6 @@ export class DynamicDataSource implements DataSource<DynamicFlatNode> {
     }
     return count;
   }
-
-  // toggleNode(node: DynamicFlatNode, expand: boolean) {
-  //   const children = this._database.getChildren(node.item);
-  //   const index = this.data.indexOf(node);
-  //   if (!children || index < 0) { // If no children, or cannot find the node, no op
-  //     return;
-  //   }
-
-  //   node.isLoading = true;
-  //   if (expand) {
-  //     const nodes = children.map((name): DynamicFlatNode => {
-  //       return new DynamicFlatNode(name, node.level + 1, this._database.isExpandable(name));
-  //     });
-  //     this.data.splice(index + 1, 0, ...nodes);
-  //   } else {
-  //     let count = 0;
-  //     for (let i = index + 1; i < this.data.length && this.data[i].level > node.level; i++, count++) { }
-  //     this.data.splice(index + 1, count);
-  //   }
-
-  //   this.dataChange.next(this.data);
-  //   node.isLoading = false;
-  // }
-
-  getSkosChildren(children: string[]) {
-    console.log("op zoek naar kinderen: ", children);
-    const queue = children.concat();
-    const child = queue.splice(0, 1).pop();
-    console.log("nu kijken naar", child);
-    // if (child) {
-    //   if (this._database.getChildren(child)?.length) {
-    //     // console.log(child, " heeft kinderen dan hoef ik niet te zoeken", children);
-    //     if (queue.length) {
-    //       console.log("verder kijken: ", queue);
-    //       this.getSkosChildren(queue);
-    //     } else {
-    //       console.log("Niets te zien hier");
-    //     }
-    //   }
-    // }
-    this.apollo.watchQuery<Query>({
-      query: GET_Concepts,
-      errorPolicy: 'all',
-      variables: { filter_broader: child },
-    }).valueChanges.pipe(map(result => {
-      // console.log("Gevonden:", result);
-      return result.data.concepts;
-    }));
-    // this.allSkosConcepts$.subscribe((data): void => {
-    //   // console.log("children data: ", data);
-    //   if (data.length) {
-    //     console.log("kindern gevonden: ", data);
-    //     if (data.length > 0 && data[0].broader?.length) {
-    //       this._database.dataMap.set(data[0].broader[0].uri, data.map(child => child.uri));
-    //     }
-    //   }
-    //   if (queue.length) {
-    //     this.getSkosChildren(queue);
-    //   } else {
-    //     console.log("done loading all children");
-    //   }
-    // });
-  }
 }
 
 @Component({
@@ -320,7 +199,6 @@ export class ConceptlistComponent implements OnInit {
   constructor(public database: DynamicDatabase, private readonly apollo: Apollo) {
     this.treeControl = new FlatTreeControl<DynamicFlatNode>(this.getLevel, this.isExpandable);
     this.dataSource = new DynamicDataSource(this.treeControl, database, apollo);
-
   }
 
   treeControl: FlatTreeControl<DynamicFlatNode>;
@@ -366,7 +244,7 @@ export class ConceptlistComponent implements OnInit {
         return conceptScheme.uri;
       });
       this.dataSource.data = this.database.initialData();
-      console.log("node map: ", this.database.nodeMap);
+      // console.log("node map: ", this.database.nodeMap);
       // console.log(this.database.dataMap);
     });
   }
